751d5aef0bf8a57368ed17e9b2246a361dcd4e42
Flore <florianmeyer67@gmail.com>
2017-08-24 17:11:08 +0200

    Add comments

--- Assets/Scripts/ComputeBufferLaunch.cs
+++ Assets/Scripts/ComputeBufferLaunch.cs
@@ -14,33 +14,55 @@ public class ComputeBufferLaunch : MonoBehaviour
 
     private void LaunchShader()
     {
+        int _shaderFunctionID = _shader.FindKernel(CustomStrings.calculateVectors);
+        
+        // Go to the Readme to understand what it means
+        int[] _dispatchSize = {2, 2, 1};
+        
+        _shader.SetInts("dispatchSize", _dispatchSize);
+        
         // What we want to be passed to the shader
         // The size is the numthreads (in the shader) * the dispatch size
-        Vector3[] _input = new Vector3[8 * 8 * 8 * 2 * 2];
+        Vector3[] _input = new Vector3[8 * 8 * 8 * _dispatchSize[0] * _dispatchSize[1] * _dispatchSize[2]];
         for (int i = 0; i < _input.Length; i++)
         {
             _input[i] = new Vector3(5.0f, 5.0f, 5.0f);
         }
         /*
          Construct the buffer
-         The 12 is the size of one element of the input (Vector3 in this case) 
-         You can now it by using System.Runtime.InteropServices.Marshal.SizeOf(new Vector3());
+         First argument is the size of the buffer
+         
+         Second argument is the size of one element of the input (Vector3 in this case) 
+         There are 2 ways to know the size : 
+         Either you know what the element is made of and you can calcute it. 
+         For instance a Vector3 contains just 3 floats so the size will be sizeof(float) * 3
+         Or you don't know what it will contain and you do System.Runtime.InteropServices.Marshal.SizeOf(new Vector3());
+         The first method is cleaner, by far
          */
         ComputeBuffer _buffer = new ComputeBuffer(_input.Length, 12);
+        //After creating the buffer we fill it
         _buffer.SetData(_input);
-        
-        print(_shader.FindKernel(CustomStrings.calculateVectors));
-        
-        _shader.SetBuffer(_shader.FindKernel(CustomStrings.calculateVectors), CustomStrings.outputBuffer, _buffer);
-        _shader.Dispatch(_shader.FindKernel(CustomStrings.calculateVectors), 2,2,1);
-        
+        /* 
+         Assign the buffer to the shader variable
+         
+         First argument is the kernel/function we want to use 
+         Second is the var name of the buffer in the shader
+         Third is the created buffer
+        */
+        _shader.SetBuffer(_shaderFunctionID, CustomStrings.outputBuffer, _buffer);
+        // Launch the compute shader kernel
+        _shader.Dispatch(_shaderFunctionID, _dispatchSize[0], _dispatchSize[1], _dispatchSize[2]);
+
+        // Get the calculated data
         _buffer.GetData(_input);
 
+        // Print them
         for (int i = 0; i < _input.Length; i++)
         {
             print(_input[i]);
         }
-        
+
+        // Release the RAM
         _buffer.Release();
     }
 }
--- Assets/Scripts/Utils/Texts.cs
+++ Assets/Scripts/Utils/Texts.cs
@@ -1,5 +1,10 @@
-﻿public static class CustomStrings
+﻿/// <summary>
+/// Contains all strings so we don't have to copy paste many times
+/// DON'T USE STRINGS BETWEEN QUOTE DIRECTLY IN YOUR CODE DAMN IT, DO AS I DO HERE !
+/// </summary>
+public static class CustomStrings
 {
     public static string calculateVectors = "CalculateVectors";
     public static string outputBuffer = "output";
+    public static string dispatchSize = "dispatchSize";
 }
